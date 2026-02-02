9d112671233b01cfb8c8eaefeccb1dd32e9566cb
Vajeeha Tabasumm <vajeehatabasumm@gmail.com>
2026-02-02 22:07:54 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Dashboard from "./components/Dashboard";
-import Admin from "./components/Admin"; // ✅ ADD THIS
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/admin" element={<Admin />} /> {/* ✅ ADD THIS */}
       </Routes>
     </BrowserRouter>
   );
